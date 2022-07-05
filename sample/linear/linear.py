8c92c98152e633bcd02034425d95dd0e7ea9de4c
frappuccino2316 <frappuccino2316@gmail.com>
2022-07-05 21:23:02 +0900

    linear_regression

---
+++ sample/linear/linear.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LinearRegression
+
+X = [[10.0], [8.0], [13.0], [9.0], [11.0], [
+    14.0], [6.0], [4.0], [12.0], [7.0], [5.0]]
+y = [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
+
+model = LinearRegression()
+model.fit(X, y)
+print(model.intercept_)
+print(model.coef_)
+y_pred = model.predict([[0], [1]])
+print(y_pred)
